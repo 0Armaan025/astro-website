7f513892257bed8f01750f4ce423fe03d6acaa7a
0Armaan025 <armaan33000@gmail.com>
2024-02-04 10:30:31 +0530

    nintendo left, right, middle and one btn done

---
+++ src/webPages/home/HomePage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './homepage.css';
+
+const HomePage = () => {
+    return (
+        <>
+            <center>
+                <br/><br/><br/>
+                <div className="nintendoSwitch">
+
+                    <div className="leftSide">
+
+                        <div className="topButton">
+                            
+                            <div className="internalButton">
+
+                            </div>
+                            
+                        </div>
+
+                        <div className="leftArrowButton"></div>
+                        <div className="rightArrowButton"></div>
+                        <div className="topArrowButton"></div>
+                        <div className="bottomArrowButton"></div>
+
+                    </div>
+                    <div className="nintendoSwitch__outsideScreen">
+                        <div className="nintendoSwitch__screen">
+                            <div className="nintendoSwitch__screen__screen">
+                            </div>
+                        </div>
+
+                        
+
+                    </div>
+
+                    <div className="rightSide">
+                        
+                        </div>
+                </div>
+            </center>
+
+        </>
+    )
+}
+
+export default HomePage
